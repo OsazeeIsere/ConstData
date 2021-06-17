53d049ba48ed9915d2fc31ab0d3f2cb4e2b0f933
Osazee Isere <oz1.concepts@gmail.com>
2021-06-18 00:29:22 +0100

    working with constant data

--- ConstData/Program.cs
+++ ConstData/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConstData
 {
+     class MyMath
+    {
+        public const double PI = 3.14;
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Fun with constant");
+            Console.WriteLine("the value of  PI is: {0}", MyMath.PI);
+            //Error! can't change a constant
+            //myMath.PI=3.45995;
+
+            Console.ReadLine();
         }
     }
 }
